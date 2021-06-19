4d37bec1f855de3777ca10a9b0fccaebb99fabdd
Robert Kan <robert.kan@gmail.com>
2021-06-19 14:40:45 -0700

    Add recipe provider and consume it

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './App.css';
+import * as rec from './RecipeProvider';
 
 /**
 * Entry point of the app
 * @return {object} react component representing the main app
 */
 function App() {
+  const recipe = useContext(rec.RecipeContext);
   return (
-    <div className="App" />
+    <rec.RecipeProvider>
+      <div className="App">
+        <h1 className="recipeName">{recipe.data.name}</h1>
+        <h2 className="ingredients">Ingredients</h2>
+        <ul>
+          {recipe.data.ingredients.map((_) => (<li>{_}</li>))}
+        </ul>
+        <h2 className="steps">Steps</h2>
+        <ul>
+          {recipe.data.steps.map((_) => (<li>{_}</li>))}
+        </ul>
+      </div>
+    </rec.RecipeProvider>
   );
 }
 
---
+++ src/RecipeProvider.jsx
@@ -0,0 +1,34 @@
+import React, { createContext, useState } from 'react';
+import PropTypes from 'prop-types';
+
+function getDefaultRecipe() {
+  return {
+    metadata: {
+      version: '1.0.0',
+    },
+    data: {
+      ingredients: ['flour', 'water', 'yeast'],
+      steps: ['Add flour, water, and yeast to a bowl', 'stir', 'backe at 350c for 30 minutes'],
+      name: ['Bread'],
+    },
+  };
+}
+
+export const RecipeContext = createContext();
+
+export function RecipeProvider({ children }) {
+  const [recipe] = useState(getDefaultRecipe);
+  return (
+    <RecipeContext.Provider value={recipe}>
+      {children}
+    </RecipeContext.Provider>
+  );
+}
+
+RecipeProvider.defaultProps = {
+  children: undefined,
+};
+
+RecipeProvider.propTypes = {
+  children: PropTypes.node,
+};
